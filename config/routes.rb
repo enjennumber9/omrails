d8decd794d6d2043b5d0a347780bc8af31ba1184
enjennumber9 <jennifer.s.c.wright@gmail.com>
2013-09-20 23:10:12 -0400

    removed extra end and work on page name error

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Omrails::Application.routes.draw do
   
   root :to => 'pages#Home'
-  get 'About' => 'pages#About'
-  get 'MyFiles' => 'pages#MyFiles'
-
+   get 'About' => 'pages#About'
+   get 'MyFiles' => 'pages#MyFiles'
 end
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -61,4 +60,4 @@ end
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
-end
+
